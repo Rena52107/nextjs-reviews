2b90df8d95634ed8609ccd7f5fa0206dc4efefa2
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-14 15:04:01 -0700

    fix typo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
-export default function RootLayout({ header, children, footer }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>{header}</header>
+        <header>[header]</header>
         <main>{children}</main>
-        <footer>{footer}</footer>
+        <footer>[footer]</footer>
       </body>
     </html>
   );
