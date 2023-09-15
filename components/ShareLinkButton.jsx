b0b9b203e90c5cbccb214f23734498dabde021af
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-15 15:34:22 -0700

    cllient component

--- components/ShareLinkButton.jsx
+++ components/ShareLinkButton.jsx
@@ -1,12 +1,16 @@
+'use client';
 
 export default function ShareLinkButton() {
   const handleClick = () => {
     console.log('clicked!');
   };
+
+  console.log('[ShareLinkButton] rendering');
+
   return (
     <button
-      className='border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700'
       onClick={handleClick}
+      className='border px-2 py-1 rounded text-slate-500 text-sm hover:bg-orange-100 hover:text-slate-700'
     >
       Share link
     </button>
