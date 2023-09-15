1720255bb78cdfb5c35c3cd812512d904adc59ca
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-15 13:25:30 -0700

    apply responsive

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,29 @@
-import Heading from "@/components/Heading";
+import Heading from '@/components/Heading';
+import Link from 'next/link';
 
 export default function HomePage() {
   console.log('[Homepage] rendering');
   return (
     <>
-     <Heading>Indie Gamer</Heading>
-      <p>Only the best indie games, reviewed for you.</p>
+      <Heading>Indie Gamer</Heading>
+      <p className='pb-3'>Only the best indie games, reviewed for you.</p>
+      <div className='bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full'>
+        <Link
+          href='/reviews/hollow-knight'
+          className='flex flex-col sm:flex-row'
+        >
+          <img
+            src='/images/hollow-knight.jpg'
+            alt='hollow-knight'
+            width='320'
+            height='180'
+            className='mb-2 rounded-t sm:rounded-l sm:rounded-r-none'
+          />
+          <h2 className='font-semibold font-orbitron py-1 text-center sm:px-2'>
+            Hollow Knight
+          </h2>
+        </Link>
+      </div>
     </>
   );
 }
