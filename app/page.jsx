1175abc6908f07e4193fd64a13a632044fb1dee0
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-15 15:02:04 -0700

    add featured review

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 import Heading from '@/components/Heading';
+import { getFeaturedReview } from '@/lib/reviews';
 import Link from 'next/link';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const review = await getFeaturedReview();
   console.log('[Homepage] rendering');
   return (
     <>
@@ -9,18 +11,18 @@ export default function HomePage() {
       <p className='pb-3'>Only the best indie games, reviewed for you.</p>
       <div className='bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full'>
         <Link
-          href='/reviews/hollow-knight'
+          href={`/reviews/${review.slug}`}
           className='flex flex-col sm:flex-row'
         >
           <img
-            src='/images/hollow-knight.jpg'
-            alt='hollow-knight'
+            src={review.image}
+            alt={review.slug}
             width='320'
             height='180'
             className='mb-2 rounded-t sm:rounded-l sm:rounded-r-none'
           />
           <h2 className='font-semibold font-orbitron py-1 text-center sm:px-2'>
-            Hollow Knight
+            {review.title}
           </h2>
         </Link>
       </div>
