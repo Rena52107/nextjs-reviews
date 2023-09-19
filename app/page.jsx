b82f4f9613656fae94629620d6a2218e7867115a
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-19 10:27:29 -0700

    display 3 featured games on Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,37 @@
+import Image from 'next/image';
 import Heading from '@/components/Heading';
-import { getFeaturedReview } from '@/lib/reviews';
+import { getReviews } from '@/lib/reviews';
 import Link from 'next/link';
 
 export default async function HomePage() {
-  const review = await getFeaturedReview();
+  const reviews = await getReviews(3);
   // console.log('[Homepage] rendering');
   return (
     <>
       <Heading>Indie Gamer</Heading>
       <p className='pb-3'>Only the best indie games, reviewed for you.</p>
-      <div className='bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full'>
-        <Link
-          href={`/reviews/${review.slug}`}
-          className='flex flex-col sm:flex-row'
-        >
-          <img
-            src={review.image}
-            alt={review.slug}
-            width='320'
-            height='180'
-            className='mb-2 rounded-t sm:rounded-l sm:rounded-r-none'
-          />
-          <h2 className='font-semibold font-orbitron py-1 text-center sm:px-2'>
-            {review.title}
-          </h2>
-        </Link>
-      </div>
+      <ul className='flex flex-col gap-3'>
+        {reviews.map((review, index) => (
+          <li className='bg-white border rounded shadow w-80 hover:shadow-xl sm:w-full'>
+            <Link
+              href={`/reviews/${review.slug}`}
+              className='flex flex-col sm:flex-row'
+            >
+              <Image
+                src={review.image}
+                alt={review.slug}
+                priority={index === 0}
+                width='320'
+                height='180'
+                className='mb-2 rounded-t sm:rounded-l sm:rounded-r-none'
+              />
+              <h2 className='font-semibold font-orbitron py-1 text-center sm:px-2'>
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
