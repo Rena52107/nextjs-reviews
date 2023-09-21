62b6c0a8620f313904981705e1a938bbe94178e6
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-20 22:22:30 -0700

    dynamic params

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,10 @@ import Link from 'next/link';
 
 export default async function HomePage() {
   const reviews = await getReviews(3);
-  console.log('[Homepage] rendering', reviews);
+  console.log(
+    '[HomePage] rendering: ',
+    reviews.map((review) => review.slug).join(', ')
+  );
   return (
     <>
       <Heading>Indie Gamer</Heading>
