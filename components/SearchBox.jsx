b99cea42ca3975f0b01de3710d2c28cfb44e406c
Rena Sakata <rena.sakata.cn@gmail.com>
2023-09-22 16:40:42 -0700

    client only rendering

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,8 +1,16 @@
 'use client';
 
+import { useIsClient } from '@/lib/hools';
 import { Combobox } from '@headlessui/react';
 
 export default function SearchBox() {
+  const isClient = useIsClient();
+
+  console.log('[SearchBox] isClient: ', isClient);
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Combobox>
       <Combobox.Input placeholder='Search...' />
